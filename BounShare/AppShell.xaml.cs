043ee426cc101a564491b5dcb9d913cb3775cb4d
sumeyyeaky156-ui <sumeyyeaky156@gmail.com>
2026-01-05 21:14:17 +0300

    Initial Project Setup

---
+++ BounShare/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BounShare;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ BounShare/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BounShare;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
